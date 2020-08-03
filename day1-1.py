ed5bad84142b2ed163501e3d643452eaa9101424
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-03 11:21:43 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:15:51 2020
+
+@author: USER
+"""
+
+print("I'm Eric")
+print ("He is cool")
+
+print("i'm Eric \nHe is 'cool'")
+input()
+
+
---
+++ day1-2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:39 2020
+
+@author: USER
+"""
+
+name=input('what is your name')
+
+print(name)
