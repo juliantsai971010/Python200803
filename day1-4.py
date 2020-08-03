9199986b63723f21de497ebb98c311d2ffcf0f01
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-03 14:17:36 +0800

    Add files via upload

---
+++ day1-4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 14:01:16 2020
+
+@author: USER
+"""
+aa=input('enter the score')
+aa=int(aa)
+if aa>=60:    
+    print('及格')
+else: 
+    print('不及格')
+    
