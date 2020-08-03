164f7c2a79780b9110a37d3c103993a62a259084
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-03 15:27:58 +0800

    Add files via upload

---
+++ day1.1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 14:56:26 2020
+
+@author: USER
+"""
+
+aa=float(input('how fat you are'))
+ab=float(input('how tallyou are '))/100
+
+BMI=aa//bb**2
+print('your BMI is',aa//bb**2)
+if BMI<18.5:
+        print('體重過輕')
+elif BMI<24:
+        print('正常')
+elif BMI<27:
+        print('過重')        
+elif BMI<30:
+        print('輕度肥胖')        
+elif BMI<35:
+        print('中度肥胖')       
+else:
+        print('重度肥胖')
+
+     
+
+
+
+
+
