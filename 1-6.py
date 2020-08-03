827d5aa4a33f477bc4cb66221d08718cc8d3901c
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-03 15:31:41 +0800

    Add files via upload

---
+++ 1-6.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 15:17:08 2020
+
+@author: USER
+"""
+aa=float(input('english'))
+ab=float(input('math'))
+if aa>90 and ab>90:
+    print('要獎勵')
+elif aa>60 or ab>60:
+    print('再加油')
+else:
+    print('要處罰')
